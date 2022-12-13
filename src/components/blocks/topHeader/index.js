8c51450fdae2979c251406fe62edfb1952144103
Chubbi-Stephen <udeokoliestephen@gmail.com>
2022-12-13 17:51:33 +0100

    Boiler-plate setup & Homepage inProgress

---
+++ src/components/blocks/topHeader/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdImg from '../../../assets/images/ads.jpg'
+
+const TopHeader = () => {
+  return (
+    <div>
+      <img src={AdImg} alt="" />
+    </div>
+  )
+}
+
+export default TopHeader
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TopHeader from '../components/blocks/topHeader'
+
+const Home = () => (
+  <div>
+    <TopHeader />
+  </div>
+)
+
+export default Home
